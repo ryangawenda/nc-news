16c3139cebf8f457eda5d4416717e5dfd46c86a6
Ryan <ryangawenda04@gmail.com>
2025-02-21 15:54:52 +0000

    Delete selected comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,37 +1,46 @@
 import { Link, useLocation, useParams } from "react-router";
-import { getComments, postComment } from "../api";
+import { getComments, postComment, deleteComment } from "../api";
 import React, { useEffect, useState } from "react";
 
 export default function Comments({ article_id, comments, setComments }) {
   const [newComment, setNewComment] = useState("");
   const [loading, setLoading] = useState(false);
+  const [deleteLoading, setdeleteLoading] = useState(false);
 
   function submitComment() {
-    if(newComment.length === 0){
-        return;
+    const commentDraft = newComment
+    setNewComment("")
+    if (commentDraft.length === 0) {
+      return;
     }
     setLoading(true);
-    postComment(article_id, newComment)
-    .then((response)=>{
-        console.log(response.data.comment)
-        console.log(comments)
+    postComment(article_id, commentDraft)
+      .then((response) => {
         setComments((prevComments) => {
-            console.log("Previous Comments:", prevComments);
-            return [response.data.comment, ...prevComments];  
+          return [response.data.comment, ...prevComments];
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function handleComment(comment_id) {
+    setdeleteLoading(true);
+    deleteComment(comment_id)
+      .then((response) => {
+        setComments((prevComments) => {
+          return prevComments.filter((comment) => {
+            return comment.comment_id !== comment_id;
           });
-    })
-    .finally(() => {
-        setLoading(false)
-
-    });
+        });
+      })
+      .finally(() => {
+        setdeleteLoading(false);
+      });
   }
+  useEffect(() => {}, [comments]);
 
-  function deleteComment(comment_id){
-    
-  }
-  useEffect(() => {
-    console.log("Updated Comments in Comments component:", comments);
-  }, [comments]);  
   return (
     <div>
       <h2>Comments</h2>
@@ -43,17 +52,23 @@ export default function Comments({ article_id, comments, setComments }) {
         className="comment-form"
       >
         <p>Want to post a comment?</p>
-        <div style={{display:"flex", alignItems:"center",justifyContent:"center" }}>
-        <input
-          type="text"
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          
-          className="comment-input"
-        />
-        <button type="submit" className="submit-button">
-          Post Comment
-        </button>{loading &&(<div className="loader"></div>)}
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <input
+            type="text"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            className="comment-input"
+          />
+          <button type="submit" className="submit-button">
+            Post Comment
+          </button>
+          {loading && <div className="loader"></div>}
         </div>
       </form>
       <ul className="comments-list">
@@ -65,6 +80,16 @@ export default function Comments({ article_id, comments, setComments }) {
             <p className="comment-body">{comment.body}</p>
             <p className="comment-date"></p>
             <p className="comment-votes">Votes: {comment.votes}</p>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleComment(comment.comment_id);
+              }}
+            >
+              <button type="submit" className="submit-button">
+                Delete Comment
+              </button>
+            </form>
           </li>
         ))}
       </ul>
